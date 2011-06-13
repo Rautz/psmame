75a84ca3267637913320e0aa2f63de8251c69c56
Mike <Mike@localhost>
2011-06-13 14:27:41 -0400

    Reorder mame drivers to ensure all clones appear in the same binary as their parent Still needs more sorting to reduce binary count / load size

--- make-mame.sh
+++ make-mame.sh
@@ -2,7 +2,7 @@
 
 mkdir -p mamesets
 
-for i in {1..23}; do
+for i in {1..15}; do
 export PS3_SET_NUM=$i
 touch src/mame/mamedriv.c
 make NOWERROR=1 CROSSBUILD=1 TARGETOS=linux OSD=sdl NO_X11=1 NO_OPENGL=1 -f makefile.ps3 -j3
