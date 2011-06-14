40547f0fe789b69cfbc38ad88534c487be86bbbc
Mike <Mike@localhost>
2011-06-13 20:42:46 -0400

    If MAME exits via error, the -showlog option is passed when returning to the frontend (no log is actually generated yet, but whatever.)

--- src/osd/sdl/sdlmain.c
+++ src/osd/sdl/sdlmain.c
@@ -408,7 +408,8 @@ int main(int argc, char *argv[])
 #else
 	if(!QuitThruSDL)
 	{
-		sys_game_process_exitspawn2("/dev_hdd0/game/MAME90000/USRDIR/frontend.self", NULL, NULL, NULL, 0, 64, SYS_PROCESS_PRIMARY_STACK_SIZE_512K);
+		const char* args[2] = {"-showlog", 0};
+		sys_game_process_exitspawn2("/dev_hdd0/game/MAME90000/USRDIR/frontend.self", (res == MAMERR_NONE) ? NULL : args, NULL, NULL, 0, 64, SYS_PROCESS_PRIMARY_STACK_SIZE_512K);
 	}
 	else
 	{
